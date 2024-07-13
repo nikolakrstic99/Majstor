bc90f37f29a5fa4ebb267ad4b5056ea0fe916aea
krsnik <nikola.krstic@ananas.rs>
2024-07-13 20:10:41 +0200

    Update read me, add master component

--- README install.txt
+++ README install.txt
@@ -2,6 +2,11 @@ FE:
 https://nodejs.org/en/download/prebuilt-installer
 npm install -g @angular/cli@13
 
+to run application, be in myMasterFE folder and run:
+      ng serve
+
+
+
 BE:
 SQL Server 9.0
 Java 17
--- myMasterFE/src/app/app.module.ts
+++ myMasterFE/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent
   ],
   imports: [
     BrowserModule,
