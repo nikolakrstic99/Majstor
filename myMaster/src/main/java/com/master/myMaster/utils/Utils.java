fe1bd97c41007a77525f0bc7898bdc51e4d9cf83
Andrej Jokic <andrej.jokic@tomtom.com>
2024-08-10 17:29:57 +0200

    add l1 and l2 categories

--- myMaster/src/main/java/com/master/myMaster/utils/Utils.java
+++ myMaster/src/main/java/com/master/myMaster/utils/Utils.java
@@ -20,7 +20,48 @@ public class Utils {
   private static final Map<String, List<String>> map = new HashMap<>();
 
   static {
-    map.put("Gradjevinski radovi", List.of("Moleraj", "Krecenje", "Keramicarski radovi", "Gipsarski radovi", "Fasaderski radovi", "Zidarski radovi", "Tesarski radovi", "Stolarski radovi", "Limarski radovi", "Vodoinstalaterski radovi", "Elektroinstalaterski radovi", "Grejanje", "Klimatizacija", "Ventilacija", "Ostali radovi"));
+    map.put(
+            "Gradjevinski radovi",
+            List.of(
+                    "Moler", "Gipsar", "Fasader", "Zidar", "Parketar", "Keramicar",
+                    "Limar", "Gradjevinski stolar", "Izolater", "Tesar",
+                    "Pomocni radnik", "Postavljanje podnih povrsina", "Secenje i busenje"
+            )
+    );
+    map.put(
+            "Elektronika",
+            List.of(
+                    "Elektricar", "Serviser lifotva", "Audio-video serviser",
+                    "Serviser mobilnih telefona", "Serviser racunara", "Serviser kucnih aparata"
+            )
+    );
+    map.put(
+            "Odrzavanje",
+            List.of(
+                    "Cistacica", "Bastovan", "Drvoseca", "Visinski radnik", "Domar"
+            )
+    );
+    map.put(
+            "Vodovod i sanitarije",
+            List.of(
+                    "Vodoinstalater", "Montazer grejnih instalacija",
+                    "Ventilacioni sistemi", "Montazer klima uredjaja", "Montazer sanitarija"
+            )
+    );
+    map.put(
+            "Obrada materijala",
+            List.of(
+                    "Bravar", "Tapetar", "Metalostrugar", "Staklorezac",
+                    "Krojac", "Obucar"
+            )
+    );
+    map.put(
+            "Automobilska industrija",
+            List.of(
+                    "Automehanicar", "Autoelektricar", "Vulkanizer", "Limar",
+                    "Slep sluzba", "Serviser motocikala"
+            )
+    );
   }
 
   public List<String> getL1Categories() {
