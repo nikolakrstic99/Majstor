f5f78d7ff48d8734ea6c15a7369dc4e88abcab6c
krsnik <nikola.krstic@ananas.rs>
2024-08-15 13:14:49 +0200

    Add ;

--- myMaster/src/main/resources/db/migration/V20240814234151__add_column_location_and_phone_number.sql
+++ myMaster/src/main/resources/db/migration/V20240814234151__add_column_location_and_phone_number.sql
@@ -1,2 +1,2 @@
-alter table users add column location varchar(255) not null default ''
+alter table users add column location varchar(255) not null default '';
 alter table users add column phone_number varchar(255) not null default '';
